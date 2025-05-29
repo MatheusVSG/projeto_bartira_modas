d4d6c486c1f8434df79342bdeaf7def9051ca38e
FabioLopes34 <fabiomusa11@gmail.com>
2025-05-29 20:57:38 -0300

    Atualizando o banco de dados

--- projeto_lab_db/loja_de_roupas (2).sql
+++ projeto_lab_db/loja_de_roupas (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27/05/2025 às 17:48
+-- Tempo de geração: 30/05/2025 às 01:54
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -40,7 +40,8 @@ CREATE TABLE `administrador` (
 INSERT INTO `administrador` (`id`, `usuario`, `senha`) VALUES
 (4, 'matheus', '$2y$10$bFnV3xf7nwQXHpInPnvPSeHLkLsk0lqF0towfdkamk89FuJ5M3wvS'),
 (5, 'carlao', '$2y$10$G0dl49FF/s798yCfAIXFkOR5qtc3TylasrYu4MjKC8BVNieW3ovxK'),
-(6, 'luan', '$2y$10$AnCmVvN1zsgPGRKwcq6TKOHarikifHt0aPjbcG7ciFgMUILElef9e');
+(6, 'luan', '$2y$10$AnCmVvN1zsgPGRKwcq6TKOHarikifHt0aPjbcG7ciFgMUILElef9e'),
+(7, 'fabio', '$2y$10$aOJ/g7L2dDpbdbl0n6cgAu2B.u5xn1ZkNFSlKqcOqoR8/hCgoDCpK');
 
 -- --------------------------------------------------------
 
@@ -71,7 +72,8 @@ CREATE TABLE `clientes` (
 
 INSERT INTO `clientes` (`id`, `nome`, `cpf`, `email`, `telefone`, `logradouro`, `numero`, `bairro`, `cidade`, `estado`, `sexo`, `data_criacao`, `data_atualizacao`, `modificado_por`) VALUES
 (4, 'Matheus Vitor Siqueira G', '22222222222', 'siqueiramatheusvitor@gmail.com', '15997562793', 'rua jose paulo colaco', '212', 'vila la brunetti', 'Itapetininga', 'sp', 'M', '2025-05-10 15:24:41', '2025-05-27 15:11:33', NULL),
-(11, 'fabio', '12344556676', 'fabio@gmail.com', '15997562793', 'rua jose paulo colaco', '212', 'vila la brunetti', 'Itapetininga', 'sp', 'M', '2025-05-27 15:36:36', '2025-05-27 15:36:36', NULL);
+(11, 'fabio', '12344556676', 'fabio@gmail.com', '15997562793', 'rua jose paulo colaco', '212', 'vila la brunetti', 'Itapetininga', 'sp', 'M', '2025-05-27 15:36:36', '2025-05-27 15:36:36', NULL),
+(12, 'fabio', '12345678910', 'fabio@gmail.com', '15', 'casa', '14', 'd', 'd', 'f', 'M', '2025-05-29 23:46:14', '2025-05-29 23:46:14', NULL);
 
 -- --------------------------------------------------------
 
@@ -80,6 +82,7 @@ INSERT INTO `clientes` (`id`, `nome`, `cpf`, `email`, `telefone`, `logradouro`, 
 --
 
 CREATE TABLE `estoque` (
+  `id` int(11) NOT NULL,
   `tamanho` varchar(10) NOT NULL,
   `fk_produto_id` int(11) DEFAULT NULL,
   `quantidade` varchar(255) DEFAULT NULL,
@@ -92,8 +95,10 @@ CREATE TABLE `estoque` (
 -- Despejando dados para a tabela `estoque`
 --
 
-INSERT INTO `estoque` (`tamanho`, `fk_produto_id`, `quantidade`, `data_de_modificacao`, `data_de_criacao`, `data_de_atualizacao`) VALUES
-('32', 1, '20', '2025-05-10 18:40:05', '2025-05-10 18:40:05', '2025-05-10 18:40:05');
+INSERT INTO `estoque` (`id`, `tamanho`, `fk_produto_id`, `quantidade`, `data_de_modificacao`, `data_de_criacao`, `data_de_atualizacao`) VALUES
+(1, '32', 1, '9', '2025-05-29 23:13:18', '2025-05-10 18:40:05', '2025-05-29 23:13:18'),
+(4, 'G', 9, '3', '2025-05-29 23:46:52', '2025-05-29 23:41:42', '2025-05-29 23:46:52'),
+(5, 'P', 9, '5', '2025-05-29 23:46:52', '2025-05-29 23:41:42', '2025-05-29 23:46:52');
 
 -- --------------------------------------------------------
 
@@ -133,7 +138,8 @@ CREATE TABLE `item_venda` (
 --
 
 INSERT INTO `item_venda` (`id`, `fk_venda_id`, `fk_produto_id`, `qtd_vendida`) VALUES
-(2, 2, 1, 60);
+(2, 2, 1, 60),
+(3, 3, 9, 1);
 
 -- --------------------------------------------------------
 
@@ -197,7 +203,9 @@ CREATE TABLE `produtos` (
 
 INSERT INTO `produtos` (`id`, `nome`, `valor_unidade`, `quantidade`, `data_criacao`, `data_atualizacao`, `modificado_por`, `foto`, `tipo_id`) VALUES
 (1, 'tenis', 1.00, 0, '2025-05-06 20:35:38', '2025-05-10 14:02:01', 0, '681f5c59eec72_Captura de tela 2025-05-10 110127.png', NULL),
-(4, 'sapato', 10.00, 0, '2025-05-09 20:29:35', '2025-05-10 14:02:13', 0, '681f5c6570c52_Captura de tela 2025-05-10 110145.png', NULL);
+(4, 'sapato', 10.00, 0, '2025-05-09 20:29:35', '2025-05-10 14:02:13', 0, '681f5c6570c52_Captura de tela 2025-05-10 110145.png', NULL),
+(8, 'calça branca ', 60.00, 0, '2025-05-29 22:27:52', '2025-05-29 22:27:52', 0, '6838df6893594.gif', 1),
+(9, 'meia marrom', 10.00, 0, '2025-05-29 23:38:30', '2025-05-29 23:41:42', 0, '6838eff600812.webp', 2);
 
 -- --------------------------------------------------------
 
@@ -206,20 +214,22 @@ INSERT INTO `produtos` (`id`, `nome`, `valor_unidade`, `quantidade`, `data_criac
 --
 
 CREATE TABLE `tipos_produto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Populando tipos de produto
-INSERT INTO `tipos_produto` (`nome`) VALUES
-('Calça'),
-('Meia'),
-('Calçado'),
-('Camiseta'),
-('Jaqueta'),
-('Short'),
-('Vestido');
+--
+-- Despejando dados para a tabela `tipos_produto`
+--
+
+INSERT INTO `tipos_produto` (`id`, `nome`) VALUES
+(1, 'Calça'),
+(2, 'Meia'),
+(3, 'Calçado'),
+(4, 'Camiseta'),
+(5, 'Jaqueta'),
+(6, 'Short'),
+(7, 'Vestido');
 
 -- --------------------------------------------------------
 
@@ -243,7 +253,8 @@ CREATE TABLE `vendas` (
 --
 
 INSERT INTO `vendas` (`id`, `fk_cliente_id`, `fk_vendedor_id`, `fk_forma_pagto_id`, `valor`, `data_criacao`, `data_venda`, `valor_total`) VALUES
-(2, 4, 2, 1, 5000, '2025-05-10 15:59:07', '2025-05-10', 0.00);
+(2, 4, 2, 1, 5000, '2025-05-10 15:59:07', '2025-05-10', 0.00),
+(3, 12, 4, 1, 10, '2025-05-29 23:46:52', '2025-05-29', 0.00);
 
 --
 -- Acionadores `vendas`
@@ -315,7 +326,8 @@ CREATE TABLE `vendedores` (
 
 INSERT INTO `vendedores` (`id`, `nome`, `cpf`, `email`, `telefone`, `logradouro`, `numero`, `bairro`, `cidade`, `estado`, `sexo`, `data_criacao`, `data_atualizacao`, `modificado_por`, `senha`, `tipo`) VALUES
 (2, 'Matheus Vitor Siqueira Gusmão', '537.724.548', 'siqueiramatheusvitor@gmail.com', '15997562793', 'rua jose paulo colaco', '212', 'vila la brunetti', 'Itapetininga', 'sp', 'M', '2025-05-10 14:43:08', '2025-05-10 14:43:08', 1, '$2y$10$jZeBwiVTCzR6kCZnfKsKhOugHoVTwL/FMqdq0BAauFTz.IQ9T9XQO', 'vendedor'),
-(3, 'luan', '12345678791', 'luan@gmail.com', '15997562793', 'rua jose paulo colaco', '212', 'vila la brunetti', 'Itapetininga', 'sp', 'M', '2025-05-27 15:30:27', '2025-05-27 15:30:27', 1, '$2y$10$E7I/mzfI3rLtKYO4fx5CfOcY9dTFyW4QamaFpdXMNa/lTv9dtZgWW', 'vendedor');
+(3, 'luan', '12345678791', 'luan@gmail.com', '15997562793', 'rua jose paulo colaco', '212', 'vila la brunetti', 'Itapetininga', 'sp', 'M', '2025-05-27 15:30:27', '2025-05-27 15:30:27', 1, '$2y$10$E7I/mzfI3rLtKYO4fx5CfOcY9dTFyW4QamaFpdXMNa/lTv9dtZgWW', 'vendedor'),
+(4, 'fabio', '12345678910', 'fabio@gmail.com', '14', 'casa', '12', 'cidade', 'bairro', 'SP', 'M', '2025-05-29 22:18:56', '2025-05-29 22:18:56', 1, '$2y$10$G6hXILrrEyjfFUEZk6GDPe/EB8St.PZKv8YiZ9z0G54Rp3q/rkJMS', 'vendedor');
 
 --
 -- Índices para tabelas despejadas
@@ -339,7 +351,7 @@ ALTER TABLE `clientes`
 -- Índices de tabela `estoque`
 --
 ALTER TABLE `estoque`
-  ADD PRIMARY KEY (`tamanho`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_produto_id` (`fk_produto_id`);
 
 --
@@ -373,6 +385,13 @@ ALTER TABLE `meta_vendas`
 -- Índices de tabela `produtos`
 --
 ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tipo_produto` (`tipo_id`);
+
+--
+-- Índices de tabela `tipos_produto`
+--
+ALTER TABLE `tipos_produto`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -399,13 +418,19 @@ ALTER TABLE `vendedores`
 -- AUTO_INCREMENT de tabela `administrador`
 --
 ALTER TABLE `administrador`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de tabela `estoque`
+--
+ALTER TABLE `estoque`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `forma_pagto`
@@ -417,7 +442,7 @@ ALTER TABLE `forma_pagto`
 -- AUTO_INCREMENT de tabela `item_venda`
 --
 ALTER TABLE `item_venda`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `logs`
@@ -435,19 +460,25 @@ ALTER TABLE `meta_vendas`
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de tabela `tipos_produto`
+--
+ALTER TABLE `tipos_produto`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `vendas`
 --
 ALTER TABLE `vendas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `vendedores`
 --
 ALTER TABLE `vendedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restrições para tabelas despejadas
@@ -473,19 +504,18 @@ ALTER TABLE `meta_vendas`
   ADD CONSTRAINT `meta_vendas_ibfk_1` FOREIGN KEY (`fk_vendedor_id`) REFERENCES `vendedores` (`id`);
 
 --
+-- Restrições para tabelas `produtos`
+--
+ALTER TABLE `produtos`
+  ADD CONSTRAINT `fk_tipo_produto` FOREIGN KEY (`tipo_id`) REFERENCES `tipos_produto` (`id`);
+
+--
 -- Restrições para tabelas `vendas`
 --
 ALTER TABLE `vendas`
   ADD CONSTRAINT `vendas_ibfk_1` FOREIGN KEY (`fk_cliente_id`) REFERENCES `clientes` (`id`),
   ADD CONSTRAINT `vendas_ibfk_2` FOREIGN KEY (`fk_vendedor_id`) REFERENCES `vendedores` (`id`),
   ADD CONSTRAINT `vendas_ibfk_3` FOREIGN KEY (`fk_forma_pagto_id`) REFERENCES `forma_pagto` (`id`);
-
---
--- Restrições para tabelas `produtos`
---
-ALTER TABLE `produtos`
-  ADD CONSTRAINT `fk_tipo_produto` FOREIGN KEY (`tipo_id`) REFERENCES `tipos_produto`(`id`);
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
