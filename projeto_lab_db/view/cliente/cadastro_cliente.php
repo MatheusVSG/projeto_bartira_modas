65bfecc2d268adb29e1e279805539e3a3d19316b
Carlos Eduardo Mimi <carloseduardomimi@gmail.com>
2025-05-29 22:31:11 -0300

    Atualização parcial da tela de cadastro de clientes do vendedor

--- projeto_lab_db/view/cliente/cadastro_cliente.php
+++ projeto_lab_db/view/cliente/cadastro_cliente.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include_once '../../connection.php';
-include '../../head.php';
 
 if (!isset($_SESSION['usuario_id']) || !in_array($_SESSION['tipo_usuario'], ['admin', 'vendedor'])) {
     header("Location: ../../login.php");
@@ -18,6 +17,8 @@ if (isset($_GET['sucesso']) && $_GET['sucesso'] == 1) {
 <html lang="pt-BR">
 
 <head>
+    <?php include '../../head.php'; ?>
+
     <title>Bartira Modas | Cadastro de Cliente</title>
     <style>
         .logo {
@@ -27,54 +28,81 @@ if (isset($_GET['sucesso']) && $_GET['sucesso'] == 1) {
     </style>
 </head>
 
-<body class="bg-dark text-light">
-    <div class="w-100 min-vh-100 d-flex flex-column justify-content-center align-items-center bg-dark p-3">
-        <div class="col-12 col-sm-8 col-md-6 col-lg-5 bg-light p-4 rounded shadow mb-4">
-            <h2 class="text-center text-dark mb-3">Cadastro de Cliente</h2>
+<body>
+    <div class="w-100 min-vh-100 bg-dark px-3 pb-3">
+        <div class="position-sticky top-0 start-0 z-3 py-3 bg-dark d-flex justify-content-end align-items-center">
+
+
+            <div class="d-flex gap-1 align-items-center">
+                <a href="listar_clientes.php" class="btn btn-primary">
+                    Clientes Cadastrados
+                </a>
+
+                <a href="../vendedor/home_vendedor.php" class="btn btn-secondary">
+                    Voltar ao Painel
+                </a>
+            </div>
+        </div>
+
+        <h4 class="text-warning mb-0">
+            Cadastro de Cliente
+        </h4>
+
+        <div class="bg-light rounded p-4">
             <?php if (!empty($mensagem_sucesso)): ?>
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
                     <?= $mensagem_sucesso ?>
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fechar"></button>
                 </div>
             <?php endif; ?>
-            <form action="../../controller/cliente/salvar_cliente.php" method="POST">
-                <div class="mb-2">
+
+            <form action=" ../../controller/cliente/salvar_cliente.php" method="POST" class="row">
+                <div class="col-12 col-lg-4 mb-3">
                     <label for="nome" class="form-label">Nome</label>
-                    <input type="text" name="nome" id="nome" required class="form-control form-control-sm" placeholder="Digite o nome">
+                    <input type="text" name="nome" id="nome" required class="form-control" placeholder="Digite o nome">
                 </div>
-                <div class="mb-2">
+
+                <div class="col-12 col-lg-2 mb-3">
                     <label for="cpf" class="form-label">CPF</label>
-                    <input type="text" name="cpf" id="cpf" required class="form-control form-control-sm" pattern="\d{11}" maxlength="11" oninput="this.value = this.value.replace(/\D/g, '')" title="Digite exatamente 11 números, somente dígitos." placeholder="Digite o CPF (somente números)">
+                    <input type="text" name="cpf" id="cpf" required class="form-control" pattern="\d{11}" maxlength="11" oninput="this.value = this.value.replace(/\D/g, '')" title="Digite exatamente 11 números, somente dígitos." placeholder="000.000.000-00">
                 </div>
-                <div class="mb-2">
+
+                <div class="col-12 col-lg-4 mb-3">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" name="email" id="email" required class="form-control form-control-sm" placeholder="Digite o e-mail">
+                    <input type="email" name="email" id="email" required placeholder="Digite o e-mail" class="form-control">
                 </div>
-                <div class="mb-2">
+
+                <div class="col-12 col-lg-2 mb-3">
                     <label for="telefone" class="form-label">Telefone</label>
-                    <input type="text" name="telefone" id="telefone" class="form-control form-control-sm" placeholder="Digite o telefone">
+                    <input type="text" name="telefone" id="telefone" class="form-control" placeholder="Digite o telefone">
                 </div>
-                <div class="mb-2">
+
+                <div class="col-11 col-lg-4 mb-3">
                     <label for="logradouro" class="form-label">Logradouro</label>
-                    <input type="text" name="logradouro" id="logradouro" class="form-control form-control-sm" placeholder="Digite o logradouro">
+                    <input type="text" name="logradouro" id="logradouro" class="form-control" placeholder="Digite o logradouro">
                 </div>
-                <div class="mb-2">
-                    <label for="numero" class="form-label">Número</label>
-                    <input type="text" name="numero" id="numero" class="form-control form-control-sm" placeholder="Digite o número">
+
+                <div class="col-2 col-lg-1 mb-3">
+                    <label for="numero" class="form-label">Nº</label>
+                    <input type="text" name="numero" id="numero" class="form-control" placeholder="Ex.: 10">
                 </div>
-                <div class="mb-2">
+
+                <div class="col-12 col-lg-3 mb-3">
                     <label for="bairro" class="form-label">Bairro</label>
-                    <input type="text" name="bairro" id="bairro" class="form-control form-control-sm" placeholder="Digite o bairro">
+                    <input type="text" name="bairro" id="bairro" class="form-control" placeholder="Digite o bairro">
                 </div>
-                <div class="mb-2">
+
+                <div class="col-12 col-lg 3 mb-3">
                     <label for="cidade" class="form-label">Cidade</label>
-                    <input type="text" name="cidade" id="cidade" class="form-control form-control-sm" placeholder="Digite a cidade">
+                    <input type="text" name="cidade" id="cidade" class="form-control" placeholder="Digite a cidade">
                 </div>
-                <div class="mb-2">
-                    <label for="estado" class="form-label">Estado (UF)</label>
-                    <input type="text" name="estado" id="estado" class="form-control form-control-sm" placeholder="Digite o estado (UF)">
+
+                <div class="col-2 col-lg-1 mb-2">
+                    <label for="estado" class="form-label">Estado</label>
+                    <input type="text" name="estado" id="estado" class="form-control" placeholder="Digite o estado (UF)">
                 </div>
-                <div class="mb-2">
+
+                <div class="mb-3">
                     <label for="sexo" class="form-label">Sexo</label>
                     <select name="sexo" id="sexo" class="form-control form-control-sm">
                         <option value="">Selecione o sexo</option>
@@ -82,35 +110,13 @@ if (isset($_GET['sucesso']) && $_GET['sucesso'] == 1) {
                         <option value="F">Feminino</option>
                     </select>
                 </div>
+
                 <div class="d-flex justify-content-end">
                     <button type="submit" name="cadastrar" class="btn btn-success btn-sm">Cadastrar</button>
                 </div>
             </form>
         </div>
-        <div class="col-12 col-sm-10 col-md-8 col-lg-7 bg-light p-4 rounded shadow">
-            <div class="d-flex justify-content-between align-items-center mb-3">
-                <h2 class="text-dark mb-0">Lista de Clientes</h2>
-                <a href="listar_clientes.php" class="btn btn-primary btn-sm">Ver Todos</a>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-bordered table-hover table-striped align-middle mb-0">
-                    <thead class="table-dark">
-                        <tr>
-                            <th>ID</th>
-                            <th>Nome</th>
-                            <th>CPF</th>
-                            <th>Email</th>
-                            <th>Telefone</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <!-- Aqui você pode inserir os dados dos clientes -->
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <a href="../administrador/home_adm.php" class="btn btn-secondary btn-sm position-fixed" style="top: 24px; right: 24px; z-index: 999;">Voltar ao Painel</a>
+
     </div>
 </body>
 
